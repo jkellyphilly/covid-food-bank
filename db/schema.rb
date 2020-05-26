12a5060fde085606efdb144e173e390e8a3530c6
jkellyphilly <joel.kelly.philly@gmail.com>
2020-05-26 18:05:56 -0400

    basic Comment model setup

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_26_215845) do
+ActiveRecord::Schema.define(version: 2020_05_26_220504) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "community_members", force: :cascade do |t|
     t.string "name"
