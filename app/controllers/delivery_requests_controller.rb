bd8986737bf26f22ca309acd6375c9edff7f6dc4
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-08 11:29:23 -0400

    basic delivery request show page

--- app/controllers/delivery_requests_controller.rb
+++ app/controllers/delivery_requests_controller.rb
@@ -1,4 +1,5 @@
 class DeliveryRequestsController < ApplicationController
+  before_action :find_request, only: [:show, :edit, :update]
 
   def index
     @delivery_requests = DeliveryRequest.all
@@ -14,7 +15,7 @@ class DeliveryRequestsController < ApplicationController
     # We can't use a hidden field to draw out the member,
     # so we have to define the association here
     @delivery_request.associateMember(session)
-    
+
     binding.pry
     if @delivery_request.valid?
       redirect_to delivery_request_path(@delivery_request)
@@ -27,6 +28,10 @@ class DeliveryRequestsController < ApplicationController
 
   def request_params
     params.require(:delivery_request).permit(:items, :requested_date)
+  end
+
+  def find_request
+    @delivery_request = DeliveryRequest.find(params[:id])
   end
 
 end
