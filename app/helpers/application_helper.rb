120d96ef7285a09400abbe7cf53e0e000de743f1
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-10 17:16:06 -0400

    further restructuring the banner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,7 +15,13 @@ module ApplicationHelper
     end
   end
 
-  def includeFlashMessage
+  def include_banner
+    if(session[:user_id])
+      render partial: "helpers/banner", locals: { user_type: session[:user_type], user_id: session[:user_id] }
+    end
+  end
+
+  def include_flash_message
     if(session[:message])
       render partial: "helpers/flash_message", locals: { message: session[:message] }
       session.delete(:message)
