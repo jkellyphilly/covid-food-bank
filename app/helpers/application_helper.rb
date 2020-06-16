7ba84b52ecd8ab486af08fc014615a0b633c4a4d
jkellyphilly <joel.kelly.philly@gmail.com>
2020-06-16 11:12:47 -0400

    removing all helpers methods from ApplicationHelper that should be elsewhere

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
 
-  def printDeliveryTitle(dr)
-    if (@community_member)
-      render partial: 'helpers/print_delivery_title', locals: { delivery_request: dr, path: community_member_delivery_request_path(@community_member, dr)}
-    else
-      render partial: 'helpers/print_delivery_title', locals: { delivery_request: dr, path: delivery_request_path(dr) }
-    end
-  end
-
   def include_banner
     if(session[:user_id])
       render partial: "helpers/banner", locals: { user_type: session[:user_type], user_id: session[:user_id] }
--- app/helpers/delivery_requests_helper.rb
+++ app/helpers/delivery_requests_helper.rb
@@ -14,6 +14,14 @@ module DeliveryRequestsHelper
     end
   end
 
+  def print_delivery_title(dr)
+    if (@community_member)
+      render partial: 'helpers/print_delivery_title', locals: { delivery_request: dr, path: community_member_delivery_request_path(@community_member, dr)}
+    else
+      render partial: 'helpers/print_delivery_title', locals: { delivery_request: dr, path: delivery_request_path(dr) }
+    end
+  end
+
   def include_sign_up_option(dr)
     if(session[:user_type] == 'volunteers')
       render partial: 'volunteer_sign_up', locals: {dr_id: dr.id}
