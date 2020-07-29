d0e5ca300771578c55299b46557aefaba02bde0b
jkellyphilly <joel.kelly.philly@gmail.com>
2020-07-29 17:43:19 -0400

    basic tests for CommunityMember

--- spec/models/community_member_spec.rb
+++ spec/models/community_member_spec.rb
@@ -4,11 +4,13 @@ require 'rails_helper'
 RSpec.describe CommunityMember, type: :model do
   before do
     @member = CommunityMember.create(name: "Testy McTestFace", username: "test 123", email: "test123@mailinator.com", address: "Testing Address", phone_number: "1234567890", allergies: "sesame", password: "test")
-    # @volunteer = Volunteer.create(name: "Testy Volunteer", username: "vol_test", email: "iluv2volunteer@gmail.com", phone_number: "1112223345", password: "test")
-    # @delivery = Delivery.new(status: "new", items: "milk and cookies", date: "Test date")
-    # @member.deliveries << @delivery
-    # @member.save
-    # @delivery.save
+    @volunteer = Volunteer.create(name: "Testy Volunteer", username: "vol_test", email: "iluv2volunteer@gmail.com", phone_number: "1112223345", password: "test")
+    @delivery_request = DeliveryRequest.new(items: "milk and cookies", requested_date: "09/19/2200")
+    @delivery_route = DeliveryRoute.new(estimated_delivery_date: "09/20/2200")
+    @volunteer.delivery_routes << @delivery_route
+    @member.delivery_requests << @delivery_request
+    @member.save
+    @delivery_request.save
   end
 
   it 'has a secure password' do
@@ -18,32 +20,36 @@ RSpec.describe CommunityMember, type: :model do
     expect(@member.authenticate("test")).to eq(@member)
   end
 
-  # it 'can keep track of all its new delivery requests' do
-  #
-  #   expect(@member.new_deliveries.size).to eq(1)
-  #   expect(@member.new_deliveries.first.items).to eq("milk and cookies")
-  # end
-  #
-  # it 'can keep track of all its confirmed delivery requests' do
-  #   @volunteer.deliveries << @delivery
-  #   @volunteer.save
-  #   @delivery.status = "confirmed"
-  #   @delivery.save
-  #
-  #   expect(@member.confirmed_deliveries.size).to eq(1)
-  #   expect(@member.confirmed_deliveries.first.items).to eq("milk and cookies")
-  #   expect(@member.new_deliveries.size).to eq(0)
-  # end
-  #
-  # it 'can keep track of all its completed delivery requests' do
-  #   @volunteer.deliveries << @delivery
-  #   @volunteer.save
-  #   @delivery.status = "completed"
-  #   @delivery.save
-  #
-  #   expect(@member.completed_deliveries.size).to eq(1)
-  #   expect(@member.completed_deliveries.first.items).to eq("milk and cookies")
-  #   expect(@member.confirmed_deliveries.size).to eq(0)
-  #   expect(@member.new_deliveries.size).to eq(0)
-  # end
+  it 'can keep track of all its new delivery requests' do
+
+    expect(@member.pending_delivery_requests.size).to eq(1)
+    expect(@member.pending_delivery_requests.first.items).to eq("milk and cookies")
+  end
+
+  it 'can keep track of all its confirmed delivery requests' do
+    @delivery_route.delivery_requests << @delivery_request
+    @volunteer.delivery_routes << @delivery_route
+    @volunteer.save
+    @delivery_route.save
+    @delivery_request.status = "confirmed"
+    @delivery_request.save
+
+    expect(@member.confirmed_delivery_requests.size).to eq(1)
+    expect(@member.confirmed_delivery_requests.first.items).to eq("milk and cookies")
+    expect(@member.pending_delivery_requests.size).to eq(0)
+  end
+
+  it 'can keep track of all its completed delivery requests' do
+    @delivery_route.delivery_requests << @delivery_request
+    @volunteer.delivery_routes << @delivery_route
+    @volunteer.save
+    @delivery_route.save
+    @delivery_request.status = "completed"
+    @delivery_request.save
+
+    expect(@member.completed_delivery_requests.size).to eq(1)
+    expect(@member.completed_delivery_requests.first.items).to eq("milk and cookies")
+    expect(@member.confirmed_delivery_requests.size).to eq(0)
+    expect(@member.pending_delivery_requests.size).to eq(0)
+  end
 end
